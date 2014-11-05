95f7f7625dee009efd86f2a0721227ce38afcd3f
thehighfish@googlemail.com <thehighfish@googlemail.com@852b20c9-6c71-199a-7a29-986ca012bdb2>
2014-11-05 20:54:45 +0000

    removed unused function for title-parsing

--- trunk/yeux_v2/CTransform/CTransform.h
+++ trunk/yeux_v2/CTransform/CTransform.h
@@ -36,11 +36,6 @@ public:
 						  CString *separation=NULL, COLORREF *cr_avg=0);
 	const int TTypeTransform(RMapCI region, const HDC hdc, CString *text, 
 							 CString *separation, bool background[], bool (*character)[MAX_CHAR_HEIGHT]);
-	const void ParseStringBSL(const CString text, const CString format, CString *results=NULL, 	
-							  double *handnumber=NULL, double *sblind=NULL, double *bblind=NULL, double *bbet=NULL, 
-							  double *ante=NULL, int *limit=NULL, double *sb_bb=NULL, double *bb_BB=NULL, bool *istournament=NULL,
-							  bool *found_handnumber=NULL, bool *found_sblind=NULL, bool *found_bblind=NULL, bool *found_bbet=NULL,
-							  bool *found_ante=NULL, bool *found_limit=NULL, bool *found_sb_bb=NULL, bool *found_bb_BB=NULL);
 	const bool IsInARGBColorCube(const int center_a, const int center_r, const int center_g, const int center_b, 
 								 const int radius, const int pix_a, const int pix_r, const int pix_g, const int pix_b);
 	const bool IsInRGBColorCube(const int center_r, const int center_g, const int center_b, 
